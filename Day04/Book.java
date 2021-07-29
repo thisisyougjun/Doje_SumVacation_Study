5e0dba968b0f839f27bfad5ef3fc8a408faa5c7a
thisisyoungjun <122899@naver.com>
2021-07-29 14:33:05 +0900

    commit

---
+++ Day04/Book.java
@@ -0,0 +1,10 @@
+package Day04;
+
+public class Book {
+    String title;
+    String author;
+    public Book(String title, String author){
+        this.title=title;
+        this.author=author;
+    }
+}
